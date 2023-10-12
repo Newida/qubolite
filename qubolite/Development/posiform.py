6991327bfbec8d75fb5e71edec188f42599b6d18
Lukas Theißinger <lukas.theissinger@iais.fraunhofer.de>
2023-10-12 18:48:56 +0200

    renamed test to posiform added file for main routine

--- qubolite/Development/posiform.py
+++ qubolite/Development/posiform.py
@@ -1,9 +1,10 @@
 import numpy as np
 from qubolite import qubo
+import igraph as ig
 
 class posiform(qubo):
     def __init__(self, q:qubo):
-        self.quadratic, self.a_0 = self.convertQUBO_to_positform(q)
+        self.matrix, self.a_0 = self.convertQUBO_to_positform(q)
         self.n = 2 * q.n
 
     def convertQUBO_to_positform(self, q: qubo):
@@ -13,9 +14,9 @@ class posiform(qubo):
         linear = np.zeros(2*n)
         linear[:n] = np.diag(q.m)
         np.fill_diagonal(Q, 0)
-        quadratic = np.zeros((2*n,2*n))
+        quadratic = np.ones((2*n,2*n))
         quadratic[:n, :n] = np.multiply(Q, Q > 0)
-        quadratic[n:, n:] = -np.multiply(Q, Q < 0)
+        quadratic[:n, n:] = -np.multiply(Q, Q < 0)
         a = np.sum(np.multiply(Q, Q < 0), axis = 1)
         linear[:n] += np.sum(np.multiply(Q, Q < 0), axis = 1)
         #Step 2: make linear terms positive
@@ -23,16 +24,20 @@ class posiform(qubo):
         linear[n:] = -linear[:n][linear[:n] < 0]
         a_0 = np.sum(linear[:n][linear[:n] < 0])
         linear[:n][linear[:n] < 0] = 0
-        np.fill_diagonal(quadratic, linear)
         return quadratic, a_0
         
 
     def convert_to_implicationnetwork(self):
-        pass
+        linear = np.diag(self.matrix)
+        #create adjacency matrix from posiform
+        print(p.matrix)
+        adjacency_matrix = np.copy((self.matrix  + self.matrix.T) / 2)
+        print(adjacency_matrix)
+
 
     def convert_toQUBO(self):
         pass
        
 Q = qubo(np.array([[-1,-2],[0,-4]]))
 p = posiform(Q)
-print(p.quadratic, p.a_0, p.n)
+#p.convert_to_implicationnetwork()
---
+++ qubolite/Development/prepro.py
@@ -0,0 +1,6 @@
+import numpy as np
+import posiform
+import igraph as ig
+
+def _network(p: posiform):
+    pass
