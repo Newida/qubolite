9b6b3d2c82aaa73e85af557dfb4670112150be5d
Lukas Theißinger <lukas.theissinger@iais.fraunhofer.de>
2023-10-16 12:27:39 +0200

    Changed posiform representation

--- qubolite/Development/posiform.py
+++ qubolite/Development/posiform.py
@@ -1,43 +1,54 @@
 import numpy as np
 from qubolite import qubo
-import igraph as ig
+import networkx as nx
+
 
 class posiform(qubo):
     def __init__(self, q:qubo):
         self.matrix, self.a_0 = self.convertQUBO_to_positform(q)
+
         self.n = 2 * q.n
 
     def convertQUBO_to_positform(self, q: qubo):
+        #create posiform in such a way that it can directly be used for the implication network
+        #therefore we add nodes x_0, not x_0 to get rid of the linear terms as described on page 5
         n = q.n
         Q = np.copy(q.m)
         #Step 1: make quadratic terms positive
         linear = np.zeros(2*n)
         linear[:n] = np.diag(q.m)
         np.fill_diagonal(Q, 0)
-        quadratic = np.ones((2*n,2*n))
-        quadratic[:n, :n] = np.multiply(Q, Q > 0)
-        quadratic[:n, n:] = -np.multiply(Q, Q < 0)
-        a = np.sum(np.multiply(Q, Q < 0), axis = 1)
-        linear[:n] += np.sum(np.multiply(Q, Q < 0), axis = 1)
-        #Step 2: make linear terms positive
-        a_0 = 0
-        linear[n:] = -linear[:n][linear[:n] < 0]
-        a_0 = np.sum(linear[:n][linear[:n] < 0])
+        quadratic = np.zeros((2*n + 2,2*n + 2))
+        positive_terms = np.multiply(Q, Q > 0)
+        negative_terms = np.multiply(Q, Q < 0)
+        #create matrix
+        quadratic[1:n+1, 1:n+1] = positive_terms
+        #quadratic[n+2:, n+2:] = positive_terms
+        #print(quadratic)
+        quadratic[1:n+1, n+2:] = -negative_terms
+        #quadratic[1:n+1, n+2:] += -negative_terms.T
+        #print(quadratic)
+        linear[:n] += np.sum(negative_terms, axis = 1)
+        #Step 2: make linear terms positive and add to x_0 and not x_0
+        linear[n:] = np.where(linear[:n] < 0, -linear[:n], 0)
+        a_0 = np.sum(linear[n:])
         linear[:n][linear[:n] < 0] = 0
+        quadratic[0, 1:n+1] = linear[:n]
+        #quadratic[n+2:, n+2] = linear[:n]
+        #print(quadratic)
+        quadratic[0, n+2:] = linear[n:]
+        #quadratic[1:n+1, n+1] = linear[n:]
+        #print(quadratic)
         return quadratic, a_0
         
 
     def convert_to_implicationnetwork(self):
-        linear = np.diag(self.matrix)
-        #create adjacency matrix from posiform
-        print(p.matrix)
-        adjacency_matrix = np.copy((self.matrix  + self.matrix.T) / 2)
-        print(adjacency_matrix)
-
+        pass
 
     def convert_toQUBO(self):
         pass
        
-Q = qubo(np.array([[-1,-2],[0,-4]]))
+Q = qubo(np.array([[2, -1, 2, -2, 2], [0, 1, 1, -1, -1], [0, 0, -2, 2, -2], [0, 0, 0, -1, 2], [0, 0, 0, 0, 1]]))
 p = posiform(Q)
+print(p.matrix)
 #p.convert_to_implicationnetwork()
