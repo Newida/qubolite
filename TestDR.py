Numerical issues are still not fixed

TestDR.py
@@ -44,8 +44,12 @@ def test_reduce_dynamic_range(maxDim, testSize=1000):
 
 #Q, Q_reduced = test_reduce_dynamic_range(3)
 
-Q  = qubo(-np.array([[-0.46402191, -0.9759945 ],
-       [ 0.        , -0.67029873]]))
+#TODO: Fix problem with numerical errors in _check_to_next_increase.
+#If the entry is close to the change the variable new_entry is extremely close to 0.
+#Sometimes the entry is -9.9e-09 and sometimes 1.1e-16, which is both extremely close to 0.
+#But the sign is appreantly extremely important, since the algorithm only insterts correctly if the sign is correct.
+Q  = qubo(np.array([[ 0.84540399,  0.50028059],
+       [ 0.        , -0.33453025]]))
 Q_reduced = reduce_dynamic_range(Q, heuristic='greedy0', decision='heuristic', iterations=100)
 print("Q_reduced: ", Q_reduced)
 truth, _ = brute_force_solutions(Q)

qubolite/preprocessing.py
@@ -380,6 +380,8 @@ def _dynamic_range_change(i, j, change, matrix_order):
     return dynamic_range_diff
 
 def _check_to_next_increase(matrix_order, change, i, j):
+    #TODO: the if clause does not work. When do i insert left and when right from 0?
+    #What does this function actually do and why?
     current_entry = matrix_order.matrix[i, j]
     new_entry = current_entry + change
     lower_index = np.searchsorted(matrix_order.unique, new_entry, side='right')
@@ -404,7 +406,7 @@ def _check_to_next_decrease(matrix_order, change, i, j):
     new_entry = current_entry + change
     upper_index = np.searchsorted(matrix_order.unique, new_entry, side='left')
     if np.isclose(new_entry, 0):
-        lower_index += 1
+        upper_index += 1
     upper_entry = matrix_order.unique[upper_index]
     min_dis = matrix_order.min_distance
     upper_interval = P.open(upper_entry - min_dis, upper_entry + min_dis)